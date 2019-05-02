4e0aaa2765fc08037b06eb703df06f2ba284f647
Christine <cmbenson88@gmail.com>
2019-05-02 13:59:20 -0500

    Add email prompting and column

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-for (let arg of process.argv){
-  if (arg([0] !== '/') {
-    console.log(arg);
-  })
-}
-=======
 const inquirer = require('inquirer');
 
 const MenuController = require('./controllers/MenuController');
@@ -12,4 +5,3 @@ const menu = new MenuController();
 
 menu.clear();
 menu.main();
->>>>>>> d3ad65552568da03b2e92c0b706708330258056e
